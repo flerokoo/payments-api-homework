16dee8e395d6ea7fd6bb847a91f8d8a0ebb75b48
Emile <emile@Emiles-MacBook-Pro.local>
2019-10-21 17:06:52 +0300

    helmet added + cleanup

--- src/configure-app.js
+++ src/configure-app.js
@@ -1,6 +1,8 @@
 let bodyParser = require("body-parser");
+let helmet = require("helmet");
 
 module.exports = ({ app }) => {
+    app.use(helmet());
     app.use(bodyParser.json());    
     app.use(bodyParser.urlencoded({ extended: true }));
 };
